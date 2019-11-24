d8698c99f07ae26d90863a6709ab4f85ca72105a
Sura Al-jilawi <suraaljailawi@gmail.com>
2019-11-24 11:06:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
-function totalBatteriesacccout(batteryBatches) {
 let totalBatteries  = 0;
 
 batteryBatches.reduce(function(element){
@@ -10,4 +9,3 @@ batteryBatches.reduce(function(element){
   
 });
 return totalBatteries;
-}
