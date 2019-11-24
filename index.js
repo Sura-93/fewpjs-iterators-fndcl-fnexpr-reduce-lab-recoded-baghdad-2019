42ad3031f12f55586ebfb6da345d3ac71a70b6b6
Sura Al-jilawi <suraaljailawi@gmail.com>
2019-11-24 11:08:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 // Code your solution here
 let totalBatteries  = 0;
 
-batteryBatches.reduce(function(element){
+batteryBatches.reduce(function(totalBatteries, element){
   
    return totalBatteries += element},0);
